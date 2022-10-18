f22d8b6853f286e44f5084469c6804b0985263a4
David <sdavidk99@gmail.com>
2022-10-19 04:52:48 +0500

    Добавил клавиатуры раздела Меню и раздела О нас

---
+++ keyboards/kb_menu.py
@@ -0,0 +1,11 @@
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton
+
+# Группировка кнопок для Меню
+menu_kb = ReplyKeyboardMarkup(resize_keyboard=True)
+menu_kb.row('Расписание на неделю','О нас')
+
+# Группировка кнопок раздела "О нас"
+
+about_us_kb = ReplyKeyboardMarkup(resize_keyboard=True)
+about_us_kb.row('Социальные сети', 'Чаты')
+about_us_kb.row('Карта церкви', 'Назад')
