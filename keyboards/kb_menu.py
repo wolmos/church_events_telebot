87d3ee0d858e9fb1dfebc2769a716cb234c1a8db
David <sdavidk99@gmail.com>
2022-12-15 16:12:23 +0300

    Добавил кнопку Пожертвование

--- keyboards/kb_menu.py
+++ keyboards/kb_menu.py
@@ -3,6 +3,7 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 # Группировка кнопок для Меню
 menu_kb = ReplyKeyboardMarkup(resize_keyboard=True)
 menu_kb.row('Расписание на неделю','О нас')
+menu_kb.row('Пожертвование')
 
 # Группировка кнопок раздела "О нас"
 
