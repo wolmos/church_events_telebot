12bd89877e433d68c7a971ec5ad70fd21df6cb81
David <sdavidk99@gmail.com>
2023-01-14 18:25:14 +0300

    Переименовал кнопку пожертвавания и убрал лишнюю

--- keyboards/inline_donate.py
+++ keyboards/inline_donate.py
@@ -1,6 +1,5 @@
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 donate_kb = InlineKeyboardMarkup()
-donate_kb.row(InlineKeyboardButton('Целевое', url='https://qr.nspk.ru/AS1A00748MOLS8598H39NKN3LFI626GB?type=01&bank=100000000111&crc=45A3', callback_data='main'))
+donate_kb.row(InlineKeyboardButton('Пожертвование', url='https://qr.nspk.ru/AS1A00748MOLS8598H39NKN3LFI626GB?type=01&bank=100000000111&crc=45A3', callback_data='main'))
 donate_kb.row(InlineKeyboardButton('Молодежный центр', url='https://qr.nspk.ru/AS1A0008SDT324SI9HCRT3GQ44TS15L0?type=01&bank=100000000111&crc=2D53', callback_data='youth'))
-donate_kb.row(InlineKeyboardButton('Пожертвовать', url='https://wolrus.org/donate', callback_data='other'))
