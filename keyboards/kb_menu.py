144c6b8cb581206c8569da8c7e065db0c535289b
SDavidKa <sdavidk99@icloud.com>
2023-02-26 05:55:44 +0300

    Временно убрал кнопку Анонсы

--- keyboards/kb_menu.py
+++ keyboards/kb_menu.py
@@ -2,13 +2,14 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
 # Группировка кнопок для Меню
 menu_kb = ReplyKeyboardMarkup(resize_keyboard=True)
-menu_kb.row('Мероприятия','О нас')
+# menu_kb.row('Мероприятия','О нас')
+menu_kb.row('Расписание на неделю','О нас')
 menu_kb.row('Пожертвовать')
 
 # Группировка кнопок раздела "Мероприятия"
 event_kb = ReplyKeyboardMarkup(resize_keyboard=True)
-event_kb.row('Расписание на неделю', 'Анонсы')
-event_kb.row('Назад')
+# event_kb.row('Расписание на неделю', 'Анонсы')
+# event_kb.row('Назад')
 
 # Группировка кнопок раздела "О нас"
 
