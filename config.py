757bc263e8dad998325ba31c784999a33688e18f
SDavidKa <sdavidk99@icloud.com>
2023-03-05 02:30:12 +0300

    Добавил подключение к БД

--- config.py
+++ config.py
@@ -1,4 +1,13 @@
 import os
 
+# НЕ ПУШИМ в гит секретные данные
+
 # Telegram bot token
 bot_token = os.environ['BOT_TOKEN']
+
+# PostgreSQL
+db_user = os.environ['DB_USER']
+db_password = os.environ['DB_PASSWORD']
+db_hostname = os.environ['DB_HOSTNAME']
+db_port = 6432
+db_name = os.environ['DB_NAME']
