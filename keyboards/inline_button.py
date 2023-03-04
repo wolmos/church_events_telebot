f25df78af42af31dcedb7d1799b4a758d14a655e
SDavidKa <sdavidk99@icloud.com>
2023-03-05 02:31:08 +0300

    Переименовал файл и добавил новую кнопку

---
+++ keyboards/inline_button.py
@@ -0,0 +1,8 @@
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+donate_kb = InlineKeyboardMarkup()
+donate_kb.row(InlineKeyboardButton('Добровольное пожертвование', url='https://qr.nspk.ru/AS1A00748MOLS8598H39NKN3LFI626GB?type=01&bank=100000000111&crc=45A3', callback_data='main'))
+donate_kb.row(InlineKeyboardButton('Целевое пожертвование', url='https://qr.nspk.ru/AS1A0008SDT324SI9HCRT3GQ44TS15L0?type=01&bank=100000000111&crc=2D53', callback_data='youth'))
+
+hg_kb = InlineKeyboardMarkup()
+hg_kb.row(InlineKeyboardButton('Оставить заявку', url='https://docs.google.com/forms/d/1tilXry9rosmP-aRHjvjY34vap906wc6tdBXNWkVnUY8/edit', callback_data='request_on_hg'))
--- keyboards/inline_donate.py
+++
@@ -1,5 +0,0 @@
-from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-donate_kb = InlineKeyboardMarkup()
-donate_kb.row(InlineKeyboardButton('Добровольное пожертвование', url='https://qr.nspk.ru/AS1A00748MOLS8598H39NKN3LFI626GB?type=01&bank=100000000111&crc=45A3', callback_data='main'))
-donate_kb.row(InlineKeyboardButton('Целевое пожертвование', url='https://qr.nspk.ru/AS1A0008SDT324SI9HCRT3GQ44TS15L0?type=01&bank=100000000111&crc=2D53', callback_data='youth'))
