06b8cb3779b23b7ac3959459255b573d29428e19
SDavidKa <sdavidk99@icloud.com>
2023-03-05 17:11:11 +0300

    Добавил новые кнопки по разделу Домашние группы

--- keyboards/kb_menu.py
+++ keyboards/kb_menu.py
@@ -1,18 +1,22 @@
 from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
-# Группировка кнопок для Меню
+# Раздел Меню
 menu_kb = ReplyKeyboardMarkup(resize_keyboard=True)
 # menu_kb.row('Мероприятия','О нас')
 menu_kb.row('Расписание на неделю','О нас')
 menu_kb.row('Пожертвовать')
 
-# Группировка кнопок раздела "Мероприятия"
+# Раздела "Мероприятия"
 event_kb = ReplyKeyboardMarkup(resize_keyboard=True)
 # event_kb.row('Расписание на неделю', 'Анонсы')
 # event_kb.row('Назад')
 
-# Группировка кнопок раздела "О нас"
+# Раздела "О нас"
 about_us_kb = ReplyKeyboardMarkup(resize_keyboard=True)
 about_us_kb.row('Социальные сети', 'Чаты')
 about_us_kb.row('Карта церкви', 'Домашние группы')
 about_us_kb.row('Назад')
+
+# Раздел "Домашние группы"
+homegroups_kb = ReplyKeyboardMarkup(resize_keyboard=True)
+homegroups_kb.row('Показать', 'Назад')
