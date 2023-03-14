1ef8f33cc01342c2ab22998857ede04a88e14c4f
SDavidKa <sdavidk99@icloud.com>
2023-03-14 15:13:59 +0300

    Вернул кнопку Мероприятия и Анонсы

--- keyboards/kb_menu.py
+++ keyboards/kb_menu.py
@@ -2,14 +2,13 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
 # Раздел Меню
 menu_kb = ReplyKeyboardMarkup(resize_keyboard=True)
-# menu_kb.row('Мероприятия','О нас')
-menu_kb.row('Расписание на неделю','О нас')
+menu_kb.row('Мероприятия','О нас')
 menu_kb.row('Пожертвовать')
 
 # Раздела "Мероприятия"
 event_kb = ReplyKeyboardMarkup(resize_keyboard=True)
-# event_kb.row('Расписание на неделю', 'Анонсы')
-# event_kb.row('Назад')
+event_kb.row('Расписание на неделю', 'Анонсы')
+event_kb.row('Назад')
 
 # Раздела "О нас"
 about_us_kb = ReplyKeyboardMarkup(resize_keyboard=True)
